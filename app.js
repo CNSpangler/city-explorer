b2df90f8e53e43d690d62264c36752e8d6d23521
Chelsea Spangler <chelseanspangler@gmail.com>
2020-02-19 10:59:28 -0800

    add weather path

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const request = require('superagent');
 const data = require('./geo.js');
+const weather = require('./darksky.js');
 const app = express();
 
 // app.get('define the path', (req is what the user requests, res is what we send back))
@@ -13,6 +14,22 @@ app.get('/location', (req, res) => {
         longitude: cityData.geometry.location.lng
     });
 });
+
+const getWeatherData = (lat, lng) => {
+    return weather.daily.data.map(forecast => {
+        return {
+            forecast: forecast.summary,
+            time: Date(forecast.time),
+        }
+    })
+}
+
+app.get('/weather', (req, res) => {
+    const portlandWeather = getWeatherData(/*lat, lng*/);
+    res.json({
+        portlandWeather
+    });
+})
 
 app.get('*', (req, res) => {
     res.json({
