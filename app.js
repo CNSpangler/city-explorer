2df6e2b017ac7028c71fdf5b4ab84b60a3afdebb
Chelsea Spangler <chelseanspangler@gmail.com>
2020-02-19 11:43:01 -0800

    getweatherdata function

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require('express');
-const request = require('superagent');
+// const request = require('superagent');
+const cors = require('cors');
 const data = require('./geo.js');
 const weather = require('./darksky.js');
 const app = express();
 
+app.use(cors());
+app.get('/', (req, res) => res.send('Welcome to City Explorer!'));
+
+let lat;
+let lng;
+
 // app.get('define the path', (req is what the user requests, res is what we send back))
 app.get('/location', (req, res) => {
+    const location = request.query.search;
     const cityData = data.results[0];
+
+    lat = cityData.geometry.location.lat,
+    lng = cityData.geometry.location.lng
 
     res.json({
         formatted_query: cityData.formatted_address,
@@ -19,17 +30,15 @@ const getWeatherData = (lat, lng) => {
     return weather.daily.data.map(forecast => {
         return {
             forecast: forecast.summary,
-            time: Date(forecast.time),
+            time: new Date(forecast.time * 1000),
         }
     })
-}
+};
 
 app.get('/weather', (req, res) => {
-    const portlandWeather = getWeatherData(/*lat, lng*/);
-    res.json({
-        portlandWeather
-    });
-})
+    const portlandWeather = getWeatherData(lat, lng);
+    res.json(portlandWeather);
+});
 
 app.get('*', (req, res) => {
     res.json({
@@ -37,4 +46,6 @@ app.get('*', (req, res) => {
     });
 });
 
-app.listen(3000, () => { console.log('running . . .'); })
+module.exports = {
+    app: app,
+};
