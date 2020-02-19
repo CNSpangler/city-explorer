dd2fc854705d9bdf9c62428f59bc55f01d8378d6
Chelsea Spangler <chelseanspangler@gmail.com>
2020-02-19 10:13:32 -0800

    push to heroku

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
-import require from 'superagent';
 const express = require('express');
-
+const request = require('superagent');
+const data = require('./geo.js');
 const app = express();
 
-app.get('/location/otherstuff', (req, res) => {
+// app.get('define the path', (req is what the user requests, res is what we send back))
+app.get('/location', (req, res) => {
+    const cityData = data.results[0];
+
     res.json({
-        key: 'value'
+        formatted_query: cityData.formatted_address,
+        latitude: cityData.geometry.location.lat,
+        longitude: cityData.geometry.location.lng
     });
 });
 
 app.get('*', (req, res) => {
     res.json({
-        ohno: '404'
+        error: '404 page not found'
     });
 });
 
-app.listen(3000, () => { console.log('running . . .'); });
+app.listen(3000, () => { console.log('running . . .'); })
